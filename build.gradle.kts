2e51747392e10785985fbbfb50b263065497da9a
Jordon de Hoog <jordon.dehoog@gmail.com>
2023-07-12 12:13:14 -0400

    add binary compatiblity

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.dokka)
     alias(libs.plugins.dependencies)
+    alias(libs.plugins.binaryCompatibility)
 }
 
 tasks.withType<DokkaMultiModuleTask>().configureEach {
